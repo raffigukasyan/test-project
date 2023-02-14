758dcd017941ddbc28bcc48ac387d18e94a0cf8b
Raffi <raffi.gukasyan777@gmail.com>
2023-02-14 16:09:51 +0300

    version1

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react"
 
 export default function Modal({active, setActive, activeElement}) {
+
+    const [value, setValue] = useState("")
+
+    const onModalExit = () => {
+        setActive(false);
+        setValue("");
+    }
     return (
-        <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
+        <div className={active ? "modal active" : "modal"} onClick={onModalExit}>
             <div className="modal__content" onClick={(e) => e.stopPropagation()}>
                 <img src={activeElement.imageUrl} />
                 <p className="title">Comments</p>
-                <textarea></textarea>
+                <textarea value={value} onChange={(evt) => setValue(evt.target.value)}></textarea>
                 <p className="subtitle">Write a few sentences about the photo.</p>
                 <button className="btnSave">Save</button>
             </div>
